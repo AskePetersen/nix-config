0d73388125e193d0aea302627177a2a12ef2fde3
Aske <aske98@live.com>
2025-12-16 21:43:22 +0100

    fixed some hardcoded paths and added some swap devices

--- hosts/ideapad/hardware-configuration.nix
+++ hosts/ideapad/hardware-configuration.nix
@@ -24,7 +24,12 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [ 
+    {
+      device = "/var/lib/swapfile";
+      size = 8192; # 8GB swap for IdeaPad (adjust based on your RAM)
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
--- hosts/thinkpad/hardware-configuration.nix
+++ hosts/thinkpad/hardware-configuration.nix
@@ -24,7 +24,12 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [ 
+    {
+      device = "/var/lib/swapfile";
+      size = 16384; # 16GB swap for ThinkPad (adjust based on your RAM)
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
